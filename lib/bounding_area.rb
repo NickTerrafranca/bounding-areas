9d5749deb1e356bfe1e6688fe5adbadf337a4980
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-06-08 10:29:01 -0400

    All tests passing.

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,21 @@
+require_relative 'bounding_box'
 class BoundingArea
+  def initialize(array)
+    @box_data = array
+    @box_results = []
+  end
+
+  def contains_point?(x, y)
+    unless @box_data.empty?
+      @box_data.each do |box|
+        if box.contains_point?(x, y)
+          return true
+        else
+          return false
+        end
+      end
+    else
+      false
+    end
+  end
 end
--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,2 +1,37 @@
 class BoundingBox
+  # attr_reader :x, :y, :w, :h
+  def initialize (x, y, w, h)
+    @x = x
+    @y = y
+    @w = w
+    @h = h
+  end
+
+  def right
+    @x + @w
+  end
+
+  def left
+    @x
+  end
+
+  def height
+    @h
+  end
+
+  def width
+    @w
+  end
+
+  def top
+    @y + @h
+  end
+
+  def bottom
+    @y
+  end
+
+  def contains_point?(x, y)
+    x >= left && x <= right && y >= bottom && y <= top
+  end
 end
